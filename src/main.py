4065b3498985d2c6fd59cd2ca3523f1ce4d5cdc3
Christos-Nikolaos Zacharopoulos <christonik@gmail.com>
2023-06-09 18:58:04 +0200

    path correction

--- src/main.py
+++ src/main.py
@@ -5,6 +5,11 @@
 """
 
 from fastapi import FastAPI
+import os
+import sys
+
+sys.path.insert(0, os.path.dirname(os.path.realpath(__file__)))
+
 from fast_api_app_creation_04 import app as application
 
 app = FastAPI()
